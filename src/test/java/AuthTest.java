5ed2ee66af6295f7a7b85639557f59eb62e910a0
IMFedorov <ultimax13@mail.ru>
2025-03-21 12:37:20 +0300

    First

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,64 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class AuthTest {
+
+    @BeforeEach
+    public void setUp() {
+        Selenide.open("http://localhost:9999");
+    }
+
+
+@Test
+public void successAuth() {
+    var registredUser = DataGenerator.Registration.getRegistredUser("active");
+
+    $("[data-test-id='login'] input").setValue(registredUser.getLogin());
+    $("[data-test-id='password'] input").setValue(registredUser.getPassword());
+    $("[data-test-id='action-login']").click();
+    $("h2")
+            .shouldBe(Condition.visible)
+            .shouldHave(Condition.text("Личный кабинет"));
+}
+
+@Test
+    public void blockedUser() {
+        var registredUser = DataGenerator.Registration.getRegistredUser("blocked");
+
+        $("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+            .shouldBe(Condition.visible)
+            .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    public void notRegistredUser() {
+        var registredUser = DataGenerator.Registration.getUser("active");
+
+        $("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    public void wrongPassword() {
+        var registredUser = DataGenerator.Registration.getRegistredUser("active");
+        var password = DataGenerator.getRandomPassword();
+
+        $("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(password);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
+    }
+}
---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,65 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+import org.apache.http.HttpStatus;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
+
+public class DataGenerator {
+    private DataGenerator(){
+
+    }
+    private static final Faker FAKER = new Faker(new Locale("en"));
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    static void setRequest(DataGenerator.UserInfo userInfo) {
+        given()
+                .spec(requestSpec)
+                .body(userInfo)
+                .when().log().all()
+                .post("/api/system/users")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    public static String getRandomLogin() {return FAKER.name().username();}
+
+    public static String getRandomPassword() {return FAKER.internet().password();}
+
+    public static class Registration {
+        private Registration(){
+
+        }
+        public static UserInfo getUser (String status){
+            return new UserInfo(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static UserInfo getRegistredUser (String status){
+            var user = getUser(status);
+            setRequest(user);
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String login;
+        String password;
+        String status;
+    }
+
+
+}
